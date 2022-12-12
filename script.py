3ede74d97ab374279c85375d1d13a2384e067bda
JY.Huang <jenyuan@stu.pku.edu.cn>
2022-12-12 20:21:09 +0800

    pre version

--- script.py
+++ script.py
@@ -1,20 +1,6 @@
-import numpy
-import torch
-from semisupervised.codes import loader
+import numpy as np
 
-# net_file = './semisupervised/data/cora' + '/net.txt'
-# vocab_node = loader.Vocab(net_file, [0, 1])
-# graph = loader.Graph(file_name=net_file, entity=[vocab_node, 0, 1])
-# graph.to_symmetric(1.0)
-# adj = graph.get_sparse_adjacency(False)
-
-a = []
-b= []
-a += [(1,2)]
-b += a
-print(type(b))
-print(len(b))
-print(b)
-for d, v in b:
-    print(d)
-    print(v)
+a = [(725,), (84,)]
+b = np.arange(84)
+c = b[-8*12:]
+print(c.reshape([12,8]))
--- semisupervised/codes/run_cora.py
+++ semisupervised/codes/run_cora.py
@@ -22,8 +22,8 @@ opt['draw'] = 'smp'
 opt['tau'] = 0.1
 opt['concat'] = 1
 opt['compare'] = 1 * opt['concat']
-opt['IR'] = 0
-opt['MC_smp'] = 1
+opt['IR'] = 1
+opt['MC_smp'] = 50
 
 def generate_command(opt):
     cmd = 'python3 train.py'
